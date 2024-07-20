17997ff6bf6e63ed4779c6d6b33c864f66e6c6ec
hsukiasyan <hakob.sukiasyan@pricespider.com>
2024-07-20 10:03:59 +0200

    add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Header from "@/app/header";
 
 export default function Home() {
   return (
-      <>
-        <Header />
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-           test
-          </div>
-        </main>
-      </>
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          test
+        </div>
+      </main>
+    </>
   );
 }
